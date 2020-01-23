673d774ae16bdf880727f5690fe580cab7aa1300
fukuda ryusei <fukuda@fukudanoiMac.local>
2020-01-23 19:11:12 +0900

    お知らせ内容の取得

---
+++ server/Parse/app/src/main/java/com/example/fukuda/parse/Parse.java
@@ -0,0 +1,93 @@
+package com.example.fukuda.parse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parse extends AppCompatActivity {
+    private static String URL = "http://52.199.105.121/SelectShopinfo.php";
+    //店舗のid
+    private static final String shopid = "gc0a608";
+    private TextView textView;
+    private TextView textView1;
+    private TextView textView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parse);
+        //内容
+        textView = findViewById(R.id.text);
+        //日
+        textView1 = findViewById(R.id.textView);
+        //時
+        textView2 = findViewById(R.id.textView2);
+        Parse(shopid);
+    }
+    private void Parse(final String shopid) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //パース
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("info");
+
+                            JSONObject object = jsonArray.getJSONObject(0);
+                            String contents = object.getString("commentcontents").trim();
+                            String date = object.getString("commentdate").trim();
+                            String time = object.getString("commenttime").trim();
+
+                            textView.setText(contents);
+                            textView1.setText(date);
+                            textView2.setText(time);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            //Toast.makeText(Parse.this, "Error" + e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Toast.makeText(Parse.this, "Error" + error.toString(), Toast.LENGTH_LONG).show();
+
+                    }
+                })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("shopid", shopid);
+                return params;
+
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+}
